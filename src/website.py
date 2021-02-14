272bf1a5e88752d2e1fb230b435640af81fd85b6
Felix Pascal Orosz <neostyxde@gmail.com>
2021-02-14 12:19:36 +0000

    random commit

--- src/website.py
+++ src/website.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 from flask import Flask, request, render_template, request
 
 cwd = os.getcwd()
-app = Flask(__name__, template_folder='html')
+app = Flask(__name__, template_folder='frontend')
 
 @app.route('/')
 def home():
@@ -20,11 +20,17 @@ def panel():
     if request.method == 'POST':
         data = request.form
         token = str(data['token'])
-        print("\n\n\n==========" + data['token'])
-        
+        print("\n\n\n====== TOKEN: " + data['token'] + " ======")
         discordbot.createbot(token)
         return render_template('panel.html', data=data)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(308)
+def permanent_redirect(e):
+    return render_template('308.html'), 308
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
